1def9375255f760a100fd4f8cff6454e194b671a
Rakesh <rakeshabhinav65@gmail.com>
2022-09-22 23:11:35 +0530

    UPDATED docs

---
+++ app.js
@@ -0,0 +1,5 @@
+var btnTranslate = document.querySelector("#btn-btnTranslate");
+var textInput = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#Output");
+
+var serverURL = 
