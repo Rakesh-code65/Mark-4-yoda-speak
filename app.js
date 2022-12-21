bfadbe5d096a2e197e483525ac4691551fccfce6
Rakesh <72179027+Rakesh-code65@users.noreply.github.com>
2022-12-21 22:07:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
@@ -24,30 +22,4 @@ const clickHandler =()=>{
     })
 };
 
-=======
-var btnTranslate = document.querySelector("#btn-translate");
-var textInput = document.querySelector("#text-input");
-var outputDiv = document.querySelector("#output");
-
-var serverURL = "https://api.funtranslations.com/translate/yoda.json";
-
-function getTranslationURL(input) {
-    return serverURL + "?" + "text=" + input;
-}
-
-const clickHandler =()=>{
-    var input= textInput.value;
-    fetch(getTranslationURL(input))
-    .then(resp=>{
-        return resp.json();})
-        .then(resp=>{var translatedText= resp.contents.translated;
-        outputDiv.innerText = translatedText;  
-
-    }).catch(err=>{
-        console.log("error", err);
-        alert("Error Occured!!! Try again");
-    })
-};
-
->>>>>>> 1446250 (test commit)
 btnTranslate.addEventListener("click", clickHandler);
